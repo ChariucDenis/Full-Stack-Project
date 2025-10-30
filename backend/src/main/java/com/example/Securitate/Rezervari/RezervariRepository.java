d2a7b6b8a849192e6bb3280b8752f26e5c7ddfcf
ChariucDenis <chariucdenis@yahoo.com>
2025-10-30 19:43:02 +0200

    AI

--- backend/src/main/java/com/example/Securitate/Rezervari/RezervariRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.Securitate.Rezervari;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface RezervariRepository extends JpaRepository<Rezervari, Long> {
-
-}
-
